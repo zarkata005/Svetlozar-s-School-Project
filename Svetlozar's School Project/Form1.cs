0c627354609cb7cad90dd57fd9cb42d8d4017168
DELL <DELL@DESKTOP-62AN73P>
2023-01-04 18:26:25 +0200

    Triangle functionality

--- Svetlozar's School Project/Form1.cs
+++ Svetlozar's School Project/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Svetlozar_s_School_Project
 {
@@ -19,7 +20,8 @@ namespace Svetlozar_s_School_Project
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Thread thread = new Thread(Triangle);
+            thread.Start();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -31,5 +33,30 @@ namespace Svetlozar_s_School_Project
         {
 
         }
+        public void Triangle()
+        {
+
+            int randomNum = new Random().Next(1, 100);
+            for (int i = 0; i < randomNum; i++)
+            {
+              
+                    Random rnd = new Random();
+                    Color randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                    Pen randomPen = new Pen(randomColor, 4);
+                    int n = new Random().Next(20, 170);
+                    PointF point1 = new PointF(new Random().Next(200, this.Width) + new Random().Next(20, 170), new Random().Next(0, this.Height - 50) + new Random().Next(20, 170));
+                    PointF point2 = new PointF(new Random().Next(200, this.Width) + new Random().Next(20, 170), new Random().Next(0, this.Height - 50) + new Random().Next(20, 170));
+                    PointF point3 = new PointF(new Random().Next(200, this.Width) + new Random().Next(20, 170), new Random().Next(0, this.Height - 50) + new Random().Next(20, 170));
+                    PointF[] curvePoints =
+                             {
+                 point1,
+                 point2,
+                 point3,
+             };
+                    this.CreateGraphics().DrawPolygon(randomPen, curvePoints); 
+                    Thread.Sleep(2000);
+                }
+            }
+          
+        }
     }
-}
