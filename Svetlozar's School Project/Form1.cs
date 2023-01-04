a31d6503c1bbb2f0b753edca8000c5a1eee92682
DELL <DELL@DESKTOP-62AN73P>
2023-01-04 18:17:45 +0200

    Added buttons

--- Svetlozar's School Project/Form1.cs
+++ Svetlozar's School Project/Form1.cs
@@ -17,5 +17,19 @@ namespace Svetlozar_s_School_Project
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
