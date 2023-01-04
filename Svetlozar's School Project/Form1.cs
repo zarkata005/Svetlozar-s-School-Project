da49faa48b2350323c0a2b757f83abd6f5cb6d4b
DELL <DELL@DESKTOP-62AN73P>
2023-01-04 18:29:50 +0200

    Rectangle functionality

--- Svetlozar's School Project/Form1.cs
+++ Svetlozar's School Project/Form1.cs
@@ -20,13 +20,14 @@ namespace Svetlozar_s_School_Project
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Thread thread = new Thread(Triangle);
-            thread.Start();
+            Thread thread1 = new Thread(Triangle);
+            thread1.Start();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            Thread thread2 = new Thread(Rectangle);
+            thread2.Start();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -39,24 +40,37 @@ namespace Svetlozar_s_School_Project
             int randomNum = new Random().Next(1, 100);
             for (int i = 0; i < randomNum; i++)
             {
-              
-                    Random rnd = new Random();
-                    Color randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
-                    Pen randomPen = new Pen(randomColor, 4);
-                    int n = new Random().Next(20, 170);
-                    PointF point1 = new PointF(new Random().Next(200, this.Width) + new Random().Next(20, 170), new Random().Next(0, this.Height - 50) + new Random().Next(20, 170));
-                    PointF point2 = new PointF(new Random().Next(200, this.Width) + new Random().Next(20, 170), new Random().Next(0, this.Height - 50) + new Random().Next(20, 170));
-                    PointF point3 = new PointF(new Random().Next(200, this.Width) + new Random().Next(20, 170), new Random().Next(0, this.Height - 50) + new Random().Next(20, 170));
-                    PointF[] curvePoints =
-                             {
+
+                Random rnd = new Random();
+                Color randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                Pen randomPen = new Pen(randomColor, 4);
+                int n = new Random().Next(20, 170);
+                PointF point1 = new PointF(new Random().Next(200, this.Width) + new Random().Next(20, 170), new Random().Next(0, this.Height - 50) + new Random().Next(20, 170));
+                PointF point2 = new PointF(new Random().Next(200, this.Width) + new Random().Next(20, 170), new Random().Next(0, this.Height - 50) + new Random().Next(20, 170));
+                PointF point3 = new PointF(new Random().Next(200, this.Width) + new Random().Next(20, 170), new Random().Next(0, this.Height - 50) + new Random().Next(20, 170));
+                PointF[] curvePoints =
+                         {
                  point1,
                  point2,
                  point3,
              };
-                    this.CreateGraphics().DrawPolygon(randomPen, curvePoints); 
-                    Thread.Sleep(2000);
-                }
+                this.CreateGraphics().DrawPolygon(randomPen, curvePoints);
+                Thread.Sleep(2000);
             }
-          
+        }
+
+        public void Rectangle()
+        {
+            int randomNum = new Random().Next(1, 100);
+            for (int i = 0; i < randomNum; i++)
+            {
+                    Random rnd = new Random();
+                    Color randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                    int b = new Random().Next(20, 170);
+                    int n = new Random().Next(20, 170);
+                    this.CreateGraphics().DrawRectangle(new Pen(randomColor, 4), new Rectangle(new Random().Next(200, this.Width), new Random().Next(0, this.Height - 50), n, b));
+                    Thread.Sleep(3000);
+            }
         }
     }
+}
